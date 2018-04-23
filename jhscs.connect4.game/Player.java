a49362dfdba39de8d791f90a70655619b187dbcc
whitec1301 <38116717+whitec1301@users.noreply.github.com>
2018-04-23 13:15:39 -0500

    Update Player.java

--- jhscs.connect4.game/Player.java
+++ jhscs.connect4.game/Player.java
@@ -1,5 +1,44 @@
 package jhscs.connect4.game;
 
+/**
+ * Abstract player, holding the basics for how players 
+ * will behave in the game 
+ * @author Cameron White
+ *
+ */
 public abstract class Player {
 
+	private AbstractPlayerController brain;
+	private char marker;
+	
+	public Player(char marker)
+	{
+		this.marker = marker;
+	}
+	
+	/**
+	 * Gets the next move from the player using the brain.getMove() function
+	 * @return - an int representing the column index of the player's move
+	 */
+	public int getMove()
+	{
+		return brain.getMove();
+	}
+
+	public AbstractPlayerController getBrain() {
+		return brain;
+	}
+
+	public void setBrain(AbstractPlayerController brain) {
+		this.brain = brain;
+	}
+
+	public char getMarker() {
+		return marker;
+	}
+
+	public void setMarker(char marker) {
+		this.marker = marker;
+	}
+	
 }
